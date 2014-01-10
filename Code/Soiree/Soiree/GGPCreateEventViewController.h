20ddcfcee7e31964332d67c22de4dee4b856febd
Greg Probst <gprobst@student.neumont.edu>
2014-01-09 18:47:41 -0700

    Beginnings of event creation

---
+++ Code/Soiree/Soiree/GGPCreateEventViewController.h
@@ -0,0 +1,13 @@
+//
+//  GGPCreateEventViewController.h
+//  Soiree
+//
+//  Created by Greg Probst on 1/9/14.
+//  Copyright (c) 2014 Greg Probst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGPCreateEventViewController : UITableViewController
+
+@end
---
+++ Code/Soiree/Soiree/GGPCreateEventViewController.m
@@ -0,0 +1,44 @@
+//
+//  GGPCreateEventViewController.m
+//  Soiree
+//
+//  Created by Greg Probst on 1/9/14.
+//  Copyright (c) 2014 Greg Probst. All rights reserved.
+//
+
+#import "GGPCreateEventViewController.h"
+
+@interface GGPCreateEventViewController ()
+
+@end
+
+@implementation GGPCreateEventViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
