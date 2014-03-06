fae968070436ebd12b68f3d969510f9ea0f35e7d
Greg Probst <gprobst@student.neumont.edu>
2014-03-06 09:52:44 -0700

    LogMode Improvements

--- Code/Soiree/Soiree/GGPLogModeViewController.h
+++ Code/Soiree/Soiree/GGPLogModeViewController.h
@@ -7,12 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "GGPLogEntry.h"
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface GGPLogModeViewController : UIViewController
+@interface GGPLogModeViewController : UIViewController<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *UIImageView;
 - (IBAction)takePhotoOrVideo:(id)sender;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UIButton *submitButton;
+@property (strong, nonatomic) UIImagePickerController *videoSelector;
+@property (strong, nonatomic) GGPLogEntry *entry;
+@property (strong, nonatomic) NSData *movData;
+@property (strong, nonatomic) UIImage *submitMovie;
 - (IBAction)submit:(id)sender;
 
 @end
--- Code/Soiree/Soiree/GGPLogModeViewController.m
+++ Code/Soiree/Soiree/GGPLogModeViewController.m
@@ -27,6 +27,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    [self.UIImageView setContentMode:UIViewContentModeScaleAspectFit];
 }
 
 - (void)didReceiveMemoryWarning
@@ -36,7 +38,44 @@
 }
 
 - (IBAction)takePhotoOrVideo:(id)sender {
+    self.videoSelector = [[UIImagePickerController alloc]init];
+    self.videoSelector.delegate = self;
+    self.videoSelector.sourceType = UIImagePickerControllerSourceTypeCamera;
+    self.videoSelector.cameraDevice = UIImagePickerControllerCameraDeviceFront;
+    
+    self.videoSelector.mediaTypes = [NSArray arrayWithObjects: (NSString *) kUTTypeMovie, kUTTypeImage, nil];
+    self.videoSelector.allowsEditing = NO;
+    [self presentViewController:self.videoSelector animated:YES completion:nil];
 }
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    if([[info objectForKey:UIImagePickerControllerMediaType] isEqualToString: (NSString *)kUTTypeMovie]){
+        NSURL *movieURL = (NSURL*)[info objectForKey:UIImagePickerControllerMediaURL];
+        self.movData = [NSData dataWithContentsOfURL:movieURL];
+        [self dismissViewControllerAnimated:YES completion:nil];
+        
+        AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:movieURL options:nil];
+        AVAssetImageGenerator *generateImg = [[AVAssetImageGenerator alloc] initWithAsset:asset];
+        NSError *error = NULL;
+        CMTime time = CMTimeMake(1, 65);
+        CGImageRef refImg = [generateImg copyCGImageAtTime:time actualTime:NULL error:&error];
+        self.UIImageView.image = [[UIImage alloc] initWithCGImage:refImg];
+        self.UIImageView.center = CGPointMake(100.0, 100.0);
+        
+        self.UIImageView.transform = CGAffineTransformMakeRotation(M_PI);
+    }else{
+        self.UIImageView.image = [info objectForKey:UIImagePickerControllerOriginalImage];
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }
+}
+
 - (IBAction)submit:(id)sender {
+    self.entry = [[GGPLogEntry alloc]init];
+    if(self.movData != nil){
+        self.entry.file = self.movData;
+        self.entry.fileType = @"MOV";
+        self.entry.isIncluded = YES;
+        self.entry.submittedBy = [PFUser currentUser].username;
+    }
 }
 @end
